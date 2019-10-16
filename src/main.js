d860b701e707de6c1b2e8afd5ef0e7279ecb626f
藍奕濡 <a78945612385238@gmail.com>
2019-10-17 00:34:55 +0800

    20191017

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue';
 import App from './App.vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import Loading from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/vue-loading.css';
+import 'bootstrap';
+import router from './router'
 
 Vue.config.productionTip = false;
+Vue.use(VueAxios, axios);
+Vue.component('Loading', Loading);
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app');
